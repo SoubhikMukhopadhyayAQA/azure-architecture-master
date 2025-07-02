eea5ab3223303bf12c99b2485dd49e3cda9437de
Soubhik M <smukhopadhyay@aqa.org.uk>
2025-07-02 07:25:27 +0100

    as

---
+++ OperationFA/DotNetAzureServicesCall/Services/ServiceInterface/IBlobService.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DotNetAzureServicesCall.Services.ServiceInterface
+{
+    public interface IBlobService
+    {
+        bool ValidateBlobContainerExists(string connectionString, string containerName);
+        (bool, string) ValidateBlobExistsInSubdirectories(string connectionString, string containerName, string directoryPath, string blobPrefix);
+    }
+}
