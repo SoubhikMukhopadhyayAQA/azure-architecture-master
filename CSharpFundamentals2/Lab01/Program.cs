409413dcfdb43df2da2e5148a076ba477a864b98
s.p.mukhopadhyay <s.p.mukhopadhyay>
2025-09-07 14:55:09 +0530

    update1.2

--- CSharpFundamentals2/Lab01/Program.cs
+++ CSharpFundamentals2/Lab01/Program.cs
@@ -1,10 +1,41 @@
 ﻿namespace Lab1
 {
+    public class Person
+    {
+        public string Name;
+
+        public void Introduce(string to)
+        {
+            Console.WriteLine("Hi {0}, I am {1}", to, Name);
+        }
+        //public Person Parse(string str)
+        //{
+        //    var person = new Person();
+        //    person.Name = str;
+        //    return person;
+        //}
+        public static Person Parse(string str)
+        {
+            var person = new Person();
+            person.Name = str;
+            return person;
+        }
+    }
     public class Program
     {
         public static void Main(string[] args)
         {
-            
+            //Person person = new Person();
+            //var person = new Person();
+
+            //var p = person.Parse("Kalle");
+
+            //person.Name = "Kalle";
+            //person.Introduce("Johanna");
+
+            var person = Person.Parse("Kalle");
+            person.Introduce("Johanna");
+
         }
     }
 }
